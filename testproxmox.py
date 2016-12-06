edeadcddc1fc243a205b858c4ec7ce3e3405130c
Nicolas Perez <git@nicolasperez.com>
2016-12-06 18:07:07 +0100

    pyproxmox

--- testproxmox.py
+++ testproxmox.py
@@ -1,9 +1,12 @@
-from pyproxmox import *
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
+from pyproxmox import *
+from pprint import pprint
 #Disable warnings
 import requests
-from requests.packages.urllib3.exceptions import InsecureRequestWarning
-requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+# from requests.packages.urllib3.exceptions import InsecureRequestWarning
+# requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 a = prox_auth('127.0.0.1','root@pam','proxmox') #local
 b = pyproxmox(a)
@@ -13,19 +16,25 @@ print(status)
 print("\n\n\nHello!\n\n\n")
 
 def creer_container_test():
-	post_data = {'ostemplate':'local:vztmpl/debian-7.0-standard_7.0-2_i386.tar.gz',
-	            'vmid':'9004','cpus':'4','description':'test container',
-	            'disk':'10','hostname':'test.example.org','memory':'1024',
-	            'password':'testPassword','swap':'1024'}
+    post_data = {'ostemplate':'local:vztmpl/debian-7.0-standard_7.0-2_i386.tar.gz',
+                'vmid':'9004','cpus':'4','description':'test container',
+                'disk':'10','hostname':'test.example.org','memory':'1024',
+                'password':'testPassword','swap':'1024'}
 
-	returndata = b.createOpenvzContainer('pve', post_data)
-	if returndata['data'] is None:
-		print('Erreur !')
-	else:
-		print('all good !')
-	print(returndata)
+    returndata = b.createOpenvzContainer('pve', post_data)
+    if returndata['data'] is None:
+        print('Erreur lors de la création du container !')
+        #Vérifier que le container dont postdata['vmid'] existe
+        
+        
+    else:
+        print('all good !')
+    print(returndata)
 
 
-print(b.getClusterVmNextId())
+pve_networkdata = b.getNodeNetworks('pve')['data'][0]
+pprint(pve_networkdata)
+node_container_index = b.getNodeContainerIndex('pve')['data'][0]
+pprint(node_container_index)
 
 
