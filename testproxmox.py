1df8648cdd42555913b5c0b2d94b3b778f19e28d
Jordy Cabannes <cabannes@etud.insa-toulouse.fr>
2016-12-12 10:19:54 +0100

    Update

--- testproxmox.py
+++ testproxmox.py
@@ -1,31 +1,40 @@
-from pyproxmox import *
+from pyproxmox.src.pyproxmox import *
 
 #Disable warnings
 import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-a = prox_auth('127.0.0.1','root@pam','proxmox') #local
+a = prox_auth('213.32.27.237','root@pam','N4rmwE3JeztE') #local
+#a = prox_auth('127.0.0.1','root@pam','rooot') #local
 b = pyproxmox(a)
+
 status = b.getClusterStatus()
 print(status)
 
 print("\n\n\nHello!\n\n\n")
 
 def creer_container_test():
-	post_data = {'ostemplate':'local:vztmpl/debian-7.0-standard_7.0-2_i386.tar.gz',
-	            'vmid':'9004','cpus':'4','description':'test container',
-	            'disk':'10','hostname':'test.example.org','memory':'1024',
-	            'password':'testPassword','swap':'1024'}
+	post_data = {
+		'vmid':'101',
+		'hostname':'test.example.org',
+		'storage':'local',
+		'password':'rooot',
+		'ostemplate':'local:vztmpl/debian-8.0-standard_8.4-1_amd64.tar.gz',
+		#'ostemplate':'local:vztmpl/debian-6.0-standard_6.0-6_i386.tar.gz',
+		'memory':'1024',
+		'swap':'1024',
+		'disk':'10',
+	    'cpus':'4',
+	}
 
-	returndata = b.createOpenvzContainer('pve', post_data)
+	returndata = b.createOpenvzContainer('ns3060138', post_data)
 	if returndata['data'] is None:
 		print('Erreur !')
 	else:
 		print('all good !')
 	print(returndata)
 
-
 print(b.getClusterVmNextId())
 
-
+creer_container_test()
