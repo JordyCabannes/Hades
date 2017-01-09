42dff3c6abf1c2e68b954534bb3026bc84fd6882
Nicolas Perez <git@nicolasperez.com>
2017-01-09 11:21:40 +0100

    debut bdd

---
+++ node-express-typescript/src/model.ts
@@ -0,0 +1,61 @@
+import {Entity, Field} from "hydrate-mongodb";
+ 
+export enum UserClass {
+    Free, 
+    Premium
+}
+ 
+@Entity()
+export class Flavor {
+ 
+    @Field()
+    name: string;
+    
+    @Field()
+    cpus: number;
+
+    @Field()
+    memory: number;
+
+    @Field()
+    disk: number;
+
+    @Field()
+    swap: number;
+
+    constructor(name: string, cpus: number, memory: number, disk: number, swap: number) {
+        this.name = name;
+        this.cpus = cpus;
+        this.memory = memory;
+        this.disk = disk;
+        this.swap = swap;
+    }
+}
+ 
+@Entity()
+export class User {
+ 
+    @Field()
+    login: string;
+ 
+    @Field()
+    password: string;
+
+    @Field()
+    user_class: UserClass;
+ 
+    @Field()
+    date_joined: Date;
+
+    @Field()
+    last_billed: Date;
+ 
+    constructor(text: string) {
+        this.login = login;
+        this.password = password;
+        this.status = user_class;
+        this.date_joined = new Date();
+        this.last_billed = new Date();
+    }
+
+}
