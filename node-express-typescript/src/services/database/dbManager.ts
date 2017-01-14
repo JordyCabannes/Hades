b8cc78595b8d7ae73640ab31f65d722f1148b03c
Nicolas Perez <git@nicolasperez.com>
2017-01-14 23:21:00 +0100

    debut_branch

--- node-express-typescript/src/services/database/dbManager.ts
+++ node-express-typescript/src/services/database/dbManager.ts
@@ -4,17 +4,13 @@ var ObjectId = require('mongodb').ObjectID;
 
 export class DBManager  
 {
-    private url :string;
+    private url: string;
 
-
-    public constructor()
-    {
-        this.url=  'mongodb://localhost:27017/hades';
-        //console.log("call dbManager constructor");
+    public constructor() {
+        this.url = 'mongodb://localhost:27017/hades';
     }
 
-    public  insert_mongodb(table: string, data: any)
-    {
+    public insert_mongodb(table: string, data: any) {
         var insertDocument = function(db, callback) {
             db.collection(table).insertOne(data, function(err, result) {
                 assert.equal(err, null);
@@ -31,23 +27,45 @@ export class DBManager
         });
     }
 
-    public ajouter_user(login: string, password: string, userType: string)
-    {
-        var data = 
-        {
+    public ajouter_user(login: string, password: string, userType: string) {
+        var data = {
             "login": login,
             "password": password,
             "typeofUser": userType,
             "date_joined": new Date(),
             "last_billed": new Date(),
             "owned_vms": []
-            //"node"
         };
         this.insert_mongodb('users', data);
     }
 
-    public ajouter_vm_a_user(login: string, id_vm: number)
-    {
+/*
+
+var vm = {
+    proxmox_id
+    node
+    dedicated
+    owner
+    date_created
+    date_deleted
+    flavor_history = [
+        date_changed
+        flavor
+        flavor_billing_rate
+    ]
+}
+
+var flavor = {
+    name
+    cpus
+    ram
+    disk
+    flavor_billing_rate
+}
+
+*/
+
+    public ajouter_vm_a_user(login: string, id_vm: number) {
         var add_vm_to_user = function (db, callback) {
             db.collection('users').update(
                 {"login": login},
@@ -68,40 +86,36 @@ export class DBManager
     }
 
 
-    public  async getTypeOfUser(userlogin : string) : Promise<string>
-    {
+    public async getTypeOfUser(userlogin: string): Promise<string> {
 
-        var functiongetuserType = async function(db)
-        {
-             var res = await db.collection('users').findOne({"login":userlogin});
+        var functiongetuserType = async function(db) {
+             var res = await db.collection('users').findOne({"login": userlogin});
              return res;
         }
 
         var db = await MongoClient.connect(this.url);
-        var result =  await functiongetuserType(db);
+        var result = await functiongetuserType(db);
+        db.close();
         return result.typeofUser;
     }
 
-    public async countUserNbVM(userlogin:string) :  Promise<number>
-    {
-        var getListVm = async function(db)
-        {
-            var res =  await db.collection('users').count({"login":userlogin});
+    public async countUserNbVM(userlogin: string): Promise<number> {
+        var getListVm = async function(db) {
+            var res = await db.collection('users').count({"login":userlogin});
               //console.log("------------------------",typeof(res.owned_vms));
             return res;
         }
 
         var db = await MongoClient.connect(this.url);
         var NbVM =  await getListVm(db);
+        db.close();
         //console.log("------------------------",typeof(PromisNbVM));
         return NbVM;
     }
 
-    /*permetd'ajouter dans la collection backup, la chemin de la backup de la vm créée par l'utilisateur login*/
-    public  associateVmBackupToAnUser(login: string,vmid:number,backupPath:string)
-    {
-        var data = 
-        {
+    /*permet d'ajouter dans la collection backup, la chemin de la backup de la vm créée par l'utilisateur login*/
+    public associateVmBackupToAnUser(login: string, vmid: number, backupPath: string) {
+        var data = {
             "login": login,
             "vmid":vmid,
             "backupPath":backupPath
@@ -110,17 +124,16 @@ export class DBManager
     }
 
     /*permet de savoir si l'utilisateur avait créé une backup pour la vm vmid*/
-    public async hasBackupAssociateWithVm(login: string,vmid:number) : Promise <boolean>
-    {
-        var functionhasBackup = async function(db)
-        {
-             var res = await db.collection('backup').count({"login":login,"vmid":vmid});
+    public async hasBackupAssociateWithVm(login: string,vmid:number): Promise <boolean> {
+        var functionhasBackup = async function(db) {
+             var res = await db.collection('backup').count({"login": login, "vmid": vmid});
              return res;
         }
 
         var db = await MongoClient.connect(this.url);
-        var result =  await functionhasBackup(db);
-        return result==1;
+        var result = await functionhasBackup(db);
+        db.close();
+        return result == 1;
     }
    
 }
